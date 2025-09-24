2810c6bb128a85605927e8ce91cf75d32ca19e23
Evellyn dos Santos Furtado <161509297+evellynfurtad0@users.noreply.github.com>
2025-09-24 17:03:58 -0300

    Add files via upload

---
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,14 @@
+import React from 'react' 
+import './TicTacToe.css';
+import circle_icon from "../Assets/circle.png"
+import cross_icon from "../Assets/cross.png"
+
+function TicTacToe() {
+  return (
+    <div>
+       <TicTacToe/>
+    </div>
+  );
+}
+
+export default TicTacToe;
